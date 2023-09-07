c70875f6f027fa1486e5bfe787ec2ba059b72427
Sundeep <sundeep.kaler@gmail.com>
2023-09-07 10:32:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(cards, giftType){
+    const thankYous = [];
+    for(let i = 0; i < cards.length; i++){
+       thankYous.push( `Thank you, ${cards[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(count){
+    while(count >=0){
+        console.log(count);
+        count--;
+    }
+}
